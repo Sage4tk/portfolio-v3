be20a323bd4b8d19ed8de453efe9061abb64b9ac
timothy timbol <ttimbol08@gmail.com>
2022-03-07 18:06:19 +0400

    added nav responsiveness with burger menu

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
+import { useState } from "react";
 
 interface Props {
     page: string
 }
 
 const Nav: React.FC<Props> = ({ page }) => {
+
+    //for mobile toggle of navbar
+    const [navBar, setNavBar] = useState(false);
+
     return(
         <nav>
-            <div>
+            <div className="burger" onClick={() => {setNavBar(!navBar)}}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className={navBar ? "nav_links active":"nav_links"}>
                 <Link href="/" >
                     <a className={page === "home" ? "nav-active":""}>Home</a>
                 </Link>
