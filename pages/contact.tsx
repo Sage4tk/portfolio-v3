49b5cf7ac7e3486fdfea12f6a0836ed292d8db13
timothy timbol <ttimbol08@gmail.com>
2022-03-10 16:08:37 +0400

    added contacts information

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -14,7 +14,21 @@ const Contacts: NextPage = () => {
                 <title>Timothy Timbol | Contacts</title>
             </Head>
             <Nav page="contact" />
-            <h1>ContactPage</h1>
+            <h1>Contact me!</h1>
+            <div className="contact-card">
+                <div>
+                    <p>GitHub:</p>
+                    <p>https://github.com/Sage4tk</p>
+                </div>
+                <div>
+                    <p>Linked-In:</p>
+                    <p>https://www.linkedin.com/in/timothy-zack-timbol-90b5271b6/</p>
+                </div>
+                <div>
+                    <p>Phone:</p>
+                    <p>+971 50 595 7395</p>
+                </div>
+            </div>
         </div>
     )
 }
