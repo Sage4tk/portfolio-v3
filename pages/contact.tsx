58ca7a054f221ed3b1652d840184febbc9853872
timothy timbol <ttimbol08@gmail.com>
2022-03-13 13:24:46 +0400

    progressive styles added to contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,33 +3,40 @@ import Head from "next/head";
 
 //components
 import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 //css
 import styles from "../styles/Contacts.module.scss";
 
 const Contacts: NextPage = () => {
     return (
-        <div className="contact-page">
+        <>
             <Head>
                 <title>Timothy Timbol | Contacts</title>
             </Head>
-            <Nav page="contact" />
-            <h1>Contact me!</h1>
-            <div className="contact-card">
-                <div>
-                    <p>GitHub:</p>
-                    <p>https://github.com/Sage4tk</p>
-                </div>
-                <div>
-                    <p>Linked-In:</p>
-                    <p>https://www.linkedin.com/in/timothy-zack-timbol-90b5271b6/</p>
-                </div>
-                <div>
-                    <p>Phone:</p>
-                    <p>+971 50 595 7395</p>
+            <div className={styles.contact_page}>
+                
+                <Nav page="contact" />
+                <h1>GET IN TOUCH!</h1>
+                <p>I am looking for any job oppurtinities, project colaboration, or any help I can give. If you have any further questions you can reach me with the means presented in this page.</p>
+                <div className="contact-card">
+                    <div>
+                        <p>E-Mail:</p>
+                        <p>ttimbol08@gmail.com</p>
+                    </div>
+                    <div>
+                        <p>Linked-In:</p>
+                        <p>https://www.linkedin.com/in/timothy-zack-timbol-90b5271b6/</p>
+                    </div>
+                    <div>
+                        <p>Phone:</p>
+                        <p>+971 50 595 7395</p>
+                    </div>
                 </div>
             </div>
-        </div>
+            <Footer />
+        </>
+        
     )
 }
 
