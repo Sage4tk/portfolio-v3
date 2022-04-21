e8f79363565e841fc99e9b8adab88679a2bd268c
timothy timbol <ttimbol08@gmail.com>
2022-04-21 19:20:06 +0400

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import '../styles/styles.scss'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script strategy="lazyOnload" src={"https://www.googletagmanager.com/gtag/js?id=G-2GCGLQ9BEQ"}/>
+      <Script strategy="lazyOnload">
+      {
+      `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-2GCGLQ9BEQ');`
+      }
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
